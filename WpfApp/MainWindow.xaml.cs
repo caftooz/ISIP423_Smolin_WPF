52eb80b3340ff0afb1f0d314c73bd51f0f270d29
caftooz <alexandersmolin07@gmail.com>
2026-01-26 15:59:30 +0300

    Add DB connection

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+            FilmListBox.ItemsSource = Core.Context.Films.ToList();
         }
     }
 }
