cb9b37a8d156152c8cf94ef54359320f291ab6f1
caftooz <alexandersmolin07@gmail.com>
2026-04-16 12:06:34 +0300

    Moved FilterChoice<T> to Core folder

---
+++ WpfApp/Core/FilterChoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    public class FilterChoice<T>
+    {
+        public T Item { get; }
+        private bool _isActive;
+        private readonly Action _onChanged;
+
+        public bool IsActive
+        {
+            get => _isActive;
+            set { _isActive = value; _onChanged?.Invoke(); }
+        }
+
+        public FilterChoice(T item, Action onChanged)
+        {
+            Item = item;
+            _onChanged = onChanged;
+        }
+    }
+}
--- WpfApp/ViewModels/ServicesViewModel.cs
+++ WpfApp/ViewModels/ServicesViewModel.cs
@@ -84,23 +84,4 @@ namespace WpfApp.ViewModels
             OnPropertyChanged(nameof(Services));
         }
     }
-
-    public class FilterChoice<T>
-    {
-        public T Item { get; }
-        private bool _isActive;
-        private readonly Action _onChanged;
-
-        public bool IsActive
-        {
-            get => _isActive;
-            set { _isActive = value; _onChanged?.Invoke(); }
-        }
-
-        public FilterChoice(T item, Action onChanged)
-        {
-            Item = item;
-            _onChanged = onChanged;
-        }
-    }
 }
